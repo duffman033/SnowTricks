25fa39a9ce2038eeb71eed0617b5b86030b2ac1f
Bastien Moreau <bmoreau72@gmail.com>
2022-10-07 11:47:01 +0200

    Ajout gestion utilisateurs et mot de passe

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ResetPasswordType extends AbstractType
+{
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'label' =>"Mot de passe",
+                'attr'=>[
+                    'placeholder'=> "Saisir votre mot de passe",
+                    'class' => 'form-control'
+                ],
+                'constraints'=> [
+                    new NotBlank([
+                        'message'=> "Nouveau mot de passe requis",
+                    ]),
+                    new Length([
+                        'min'=> 6,
+                        'minMessage'=> "Minimum de 6 caractères",
+                        'max'=> 32,
+                        'maxMessage'=> "Maximum de 32 caractères",
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
---
+++ src/Service/MailerService.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Service;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
+class MailerService
+{
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * Send email
+     *
+     * @param array $destination
+     * @param array $parameters
+     * @return void
+     */
+    public function send(array $destination, array $parameters)
+    {
+        $email = (new TemplatedEmail())
+            ->from($destination["from"])
+            ->to($destination["to"])
+            ->subject($destination["subject"])
+            ->htmlTemplate($destination["template"])
+            ->context($parameters);
+
+        $this->mailer->send($email);
+    }
+}
